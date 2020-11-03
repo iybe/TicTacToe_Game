0ab1dbe3ba7711f73f4502479cbd121e308257fc
iybe <iesleybezerra2@gmail.com>
2020-11-03 20:23:14 -0300

    style: styles of tags of main page

--- src/pages/Game/style.ts
+++ src/pages/Game/style.ts
@@ -1,4 +1,27 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+
+interface ICelulaData {
+  bordas: number;
+}
+
+export const Header = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  p {
+    font-size: 40px;
+    font-family: Arial, Helvetica, sans-serif;
+    margin-bottom: 10px;
+  }
+
+  button {
+    border-radius: 10px;
+    padding: 7px 7px;
+    font-size: 20px;
+    margin-bottom: 10px;
+  }
+`;
 
 export const Content = styled.div`
   width: 100%;
@@ -26,14 +49,33 @@ export const LinhaTabuleiro = styled.div`
   width: 600px;
 `;
 
-export const Celula = styled.div`
-  background-color: #e6e6e6;
+export const Celula = styled.div<ICelulaData>`
+  background-color: white;
   width: 180px;
   height: 180px;
-
-  align-items: center;
-  font-size: 180px;
   color: black;
-
   border-radius: 20px;
+
+  ${props =>
+    props.bordas === 1
+      ? css`
+          border-color: green;
+          border-style: solid;
+          border-width: 7px;
+        `
+      : ''}
+
+  img {
+    width: 160px;
+    height: 160px;
+
+    ${props =>
+      props.bordas === 1
+        ? css`
+            padding: 10px 10px;
+          `
+        : css`
+            margin: 10px 10px;
+          `}
+  }
 `;
